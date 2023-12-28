1a4953f8d2572d4441c407a22238495f84f58159
tomwiet <tthanquol@gmail.com>
2023-12-28 16:31:24 +0100

    Added sending mail method

---
+++ ReportService/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReportService.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string StripHTML(this string input) 
+        {
+            return Regex.Replace(input, "<.*?>", string.Empty);
+        }
+
+    }
+}
---
+++ ReportService/Models/EmailParams.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportService.Models
+{
+    public class EmailParams
+    {
+        public string HostSmtp { get; set; }
+        public int Port { get; set; }
+        public bool EnableSsl { get; set; }
+        public string SenderEmail { get; set; }
+        public string SenderEmailPassword { get; set; }
+        public string senderName { get; set; }
+    }
+}
